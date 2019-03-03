af9b97f948ac79b5ef255939bb3900dee940854f
Chris <cfd19@hotmail.co.nz>
2019-03-03 14:07:03 +1300

    Simplified and tidied and added Promises

--- pinnacleAPI.js
+++ pinnacleAPI.js
@@ -6,115 +6,52 @@ var qs = require('qs');
 var operations = require('./pinnacleOperations');
 
 var PinnacleAPI = function(username, password) {
-	if(!username || !password){
+	if (!username || !password){
 		throw new Error('No username and/or password provided in createClient().');
 	}
-	var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
 
-	var setOperation = (function(operation){
-		this.operation = operation;
-	}).bind(this);
+	var auth = `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`;
 
-	var buildUrl = (function() {
-		if (this.environment === 'development') {
-			this.url = operations[this.operation].devUrl;
-		}
-		else {
-			this.url = 'https://api.pinnacle.com/' + operations[this.operation].version + '/' + operations[this.operation].endpoint;
-		}
-	}).bind(this);
+	this.request = (endpoint, options) => {
+		return new Promise((resolve, reject) => {
+			const operation = operations[endpoint];
 
-	var checkRequired = (function(options) {
-		var required = operations[this.operation].required;
-		if (required) {
-			required.forEach((function(key) {
-				if (_.isUndefined(options[key])) {
-					throw new Error('The ' + this.operation + ' operation requires the following request data: {' + required.join(', ') + '}.');
-				}
-			}).bind(this));
-		}
-	}).bind(this);
-
-	var get = (function(options, cb) {
-		var requestOptions = {
-			url: this.url + '?' + qs.stringify(options),
-			rejectUnauthorized: false,
-			headers: {
-				'Authorization': auth
+			if (_.isUndefined(operation)) {
+				reject(`Endpoint ${endpoint} is missing from operations`);
 			}
-		};
-		request.get(requestOptions, (function (err, response, body) {
-			if (err) return cb(err);
-			if (!body || _.isEmpty(JSON.parse(body))) return cb(null, '');
-			parse(body, function(err, parsed) {
-				if (err) return cb(err);
-				cb(null, response, parsed);
+
+			(operation.required || []).forEach(key => {
+				if (_.isUndefined(options[key])) {
+					reject(`${key} is required and is missing from options`);
+				}
 			});
-		}).bind(this));
-	}).bind(this);
 
-	var parse = function(body, cb) {
-		try {
-			var result = JSON.parse(body);
-			return cb(null, result);
-		}
-		catch(e){
-			return cb(e + ', Data: ' + body);
-		}
-	}
+			const url = `https://api.pinnacle.com/${operation.version}/${operation.endpoint}?${qs.stringify(options)}`
+			var requestOptions = {
+				url,
+				rejectUnauthorized: false,
+				headers: {
+					'Authorization': auth
+				}
+			};
 
-	this.setEnvironmentDev = function(){
-		this.environment = 'development';
-	}
+			request[operation.type](requestOptions, (err, response, body) => {
+				if (err) {
+					reject(err);
+				}
 
-	this.getSports = function(cb) {
-		var options = {};
-		setOperation('getSports');
-		buildUrl();
-		get(options, cb);
-	}
+				if (!body) {
+					resolve();
+				}
 
-	this.getLeagues = function(options, cb) {
-		setOperation('getLeagues');
-		buildUrl();
-		checkRequired(options, cb);
-		get(options, cb);
-	}
-
-	this.getFixtures = function(options, cb) {
-		setOperation('getFixtures');
-		buildUrl();
-		checkRequired(options, cb);
-		get(options, cb);
-	}
-
-	this.getSettledFixtures = function(options, cb) {
-		this.operation = 'getSettledFixtures';
-		buildUrl();
-		checkRequired(options, cb);
-		get(options, cb);
-	}
-
-	this.getTeaserGroups = function(options, cb) {
-		this.operation = 'getTeaserGroups';
-		buildUrl();
-		checkRequired(options, cb);
-		get(options, cb);
-	}
-
-	this.getOdds = function(options, cb) {
-		this.operation = 'getOdds';
-		buildUrl();
-		checkRequired(options, cb);
-		get(options, cb);
-	}
-
-	this.getCurrencies = function(options, cb) {
-		this.operation = 'getCurrencies';
-		buildUrl();
-		checkRequired(options, cb);
-		get(options, cb);
-	}
+				try {
+					resolve(JSON.stringify(response));
+				} catch (e) {
+					reject(e);
+				}
+			});
+		});
+	};
 }
 
 module.exports = PinnacleAPI;
--- pinnacleOperations.js
+++ pinnacleOperations.js
@@ -1,132 +1,164 @@
 'use strict';
 
 var operations = {
-	getSports: {
-		type: 'get',
-		version: 'v2',
-		endpoint: 'sports',
-		devUrl: 'http://www.mocky.io/v2/571995d41200006e031b7192'
-	},
-	getLeagues: {
-		type: 'get',
-		version: 'v2',
-		endpoint: 'leagues',
-		required: ['sportId'],
-		devUrl: 'http://www.mocky.io/v2/572163860f0000621a387386'
-	},
-	getFixtures: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'fixtures',
-		required: ['sportId'],
-		devUrl: 'http://www.mocky.io/v2/57228c2e1200009c053e7653'
-	},
-	getSettledFixtures: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'fixtures/settled',
-		required: ['sportId'],
-		devUrl: 'http://www.mocky.io/v2/5723a0c7250000ed07e2ed2d'
-	},
-	getTeaserGroups: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'teaser/groups',
-		required: ['oddsFormat'],
-		devUrl: 'http://www.mocky.io/v2/5723db54250000bb11e2ed67'
-	},
-	getOdds: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'odds',
-		required: ['sportId'],
-		devUrl: 'http://www.mocky.io/v2/57ba024b0f00004b1dfcc923'
-	},
-	getParlayOdds: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'odds/parlay',
-		required: ['sportId'],
-		devUrl: 'http://www.mocky.io/v2/5726d508100000432f6dd6ac'
-	},
-	getTeaserOdds: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'odds/teaser',
-		required: ['teaserid'],
-		devUrl: 'http://www.mocky.io/v2/572800cb120000b40cc05a2d'
-	},
-	getCurrencies: {
-		type: 'get',
-		version: 'v2',
-		endpoint: 'currencies',
-		devUrl: 'http://www.mocky.io/v2/5728019f120000d40cc05a2e'
-	},
+	// Customer API
 	getClientBalance: {
 		type: 'get',
 		version: 'v1',
-		endpoint: 'client/balance',
-		devUrl: 'http://www.mocky.io/v2/57280237120000e90cc05a2f'
-	},
-	getLine: {
-		version: 'v1',
-		endpoint: 'line',
-		required: ['sportId', 'leagueid', 'eventid', 'periodNumber', 'betType', 'oddsFormat'],
-		devUrl: 'http://www.mocky.io/v2/57280237120000e90cc05a2f'
-	},
-	getParlayLines: {
-		type: 'post',
-		version: 'v1',
-		endpoint: 'line/parlay',
-		required: ['oddsFormat', 'legs'],
-		devUrl: 'http://www.mocky.io/v2/57280b01120000cc0dc05a32'
-	},
-	getTeaserLines: {
-		type: 'post',
-		version: 'v1',
-		endpoint: 'line/teaser',
-		required: ['teaserid', 'oddsFormat', 'legs'],
-		devUrl: 'http://www.mocky.io/v2/57280b53120000c50dc05a33'
-	},
-	placeBet: {
-		type: 'post',
-		version: 'v1',
-		endpoint: 'bets/place',
-		required: ['uniqueRequestId', 'acceptBetterLine', 'oddsFormat', 'stake', 'winRiskStake', 'sportId', 'eventId', 'periodNumber', 'betType', 'lineId'],
-		devUrl: 'http://www.mocky.io/v2/57280d18120000f30dc05a39'
-	},
-	placeParlayBet: {
-		type: 'post',
-		version: 'v1',
-		endpoint: 'bets/parlay',
-		required: ['uniqueRequestId', 'acceptBetterLine', 'oddsFormat', 'riskAmount', 'roundRobinOptions', 'legs'],
-		devUrl: 'http://www.mocky.io/v2/57280db5120000040ec05a3a'
-	},
-	placeTeaserBet: {
-		type: 'post',
-		version: 'v1',
-		endpoint: 'bets/teaser',
-		required: ['uniqueRequestId', 'teaserId', 'oddsFormat', 'winRiskStake', 'stake', 'legs'],
-		devUrl: 'http://www.mocky.io/v2/57280e72120000300ec05a3d'
-	},
-	getBets: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'bets',
-		devUrl: 'http://www.mocky.io/v2/572811041200008f0ec05a40'
-	},
-	getInrunning: {
-		type: 'get',
-		version: 'v1',
-		endpoint: 'inrunning',
-		devUrl: 'http://www.mocky.io/v2/5728139b120000cd0ec05a43'
+		endpoint: 'client/balance'
 	},
 	getTranslations: {
 		type: 'get',
 		version: 'v1',
 		endpoint: 'translations',
-		required: ['cultureCodes', 'baseTexts'],
-		devUrl: 'http://www.mocky.io/v2/57281276120000b90ec05a42'
+		required: ['cultureCodes', 'baseTexts']
+	},
+
+	// Lines API
+	getFixtures: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'fixtures',
+		required: ['sportId']
+	},
+	getSpecialFixtures: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'fixtures/special',
+		required: ['sportId']
+	},
+	getSettledFixtures: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'fixtures/settled',
+		required: ['sportId']
+	},
+	getSettledSpecialFixtures: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'fixtures/special/settled',
+		required: ['sportId']
+	},
+	getOdds: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'odds',
+		required: ['sportId']
+	},
+	getParlayOdds: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'odds/parlay',
+		required: ['sportId']
+	},
+	getTeaserOdds: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'odds/teaser',
+		required: ['teaserid']
+	},
+	getSpecialOdds: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'odds/special',
+		required: ['sportId']
+	},
+	getLine: {
+		version: 'v1',
+		endpoint: 'line',
+		required: ['sportId', 'leagueid', 'eventid', 'periodNumber', 'betType', 'oddsFormat']
+	},
+	getParlayLines: {
+		type: 'post',
+		version: 'v1',
+		endpoint: 'line/parlay',
+		required: ['oddsFormat', 'legs']
+	},
+	getTeaserLines: {
+		type: 'post',
+		version: 'v1',
+		endpoint: 'line/teaser',
+		required: ['teaserid', 'oddsFormat', 'legs']
+	},
+	getSpecialLines: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'line/special',
+		required: ['specialId', 'oddsFormat', 'contestantId']
+	},
+	getSports: {
+		type: 'get',
+		version: 'v2',
+		endpoint: 'sports'
+	},
+	getLeagues: {
+		type: 'get',
+		version: 'v2',
+		endpoint: 'leagues',
+		required: ['sportId']
+	},
+	getPeriods: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'periods',
+		required: ['sportId']
+	},
+	getInrunning: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'inrunning'
+	},
+	getTeaserGroups: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'teaser/groups',
+		required: ['oddsFormat']
+	},
+	getCancellationReasons: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'cancellationreasons'
+	},
+	getCurrencies: {
+		type: 'get',
+		version: 'v2',
+		endpoint: 'currencies'
+	},
+
+	// Bets API
+	placeBet: {
+		type: 'post',
+		version: 'v2',
+		endpoint: 'bets/straight',
+		required: ['uniqueRequestId', 'acceptBetterLine', 'oddsFormat', 'stake', 'winRiskStake', 'sportId', 'eventId', 'periodNumber', 'betType', 'lineId']
+	},
+	placeParlayBet: {
+		type: 'post',
+		version: 'v1',
+		endpoint: 'bets/parlay',
+		required: ['uniqueRequestId', 'acceptBetterLine', 'oddsFormat', 'riskAmount', 'roundRobinOptions', 'legs']
+	},
+	placeTeaserBet: {
+		type: 'post',
+		version: 'v1',
+		endpoint: 'bets/teaser',
+		required: ['uniqueRequestId', 'teaserId', 'oddsFormat', 'winRiskStake', 'stake', 'legs']
+	},
+	placeSpecialBet: {
+		type: 'post',
+		version: 'v1',
+		endpoint: 'bets/special',
+		required: ['bets']
+	},
+	getBets: {
+		type: 'get',
+		version: 'v3',
+		endpoint: 'bets'
+	},
+	getBettingStatus: {
+		type: 'get',
+		version: 'v1',
+		endpoint: 'bets/betting-status'
 	}
 };
 
